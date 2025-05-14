82336a98b3881a18518dd9486d64f4f8516e8e8b
Linus Bittner <linus@solt-bittner.at>
2025-05-14 21:50:21 +0200

    java project setup and fixes

---
+++ FlowSpace_ProjectFolder/src/main/java/com/example/flowspace_projectfolder/Main.java
@@ -0,0 +1,16 @@
+package com.example.flowspace_projectfolder;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Flowspace");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
