25f49ac33dbdb1759ee57f6e2a49218870349dba
Xwuidudl <104171032+Xwuidudl@users.noreply.github.com>
2025-05-23 07:58:53 +0200

    Client-calendar functioning dates

--- FlowSpace_Client/src/main/java/com/example/flowspace_projectfolder/CalenderView.java
+++ FlowSpace_Client/src/main/java/com/example/flowspace_projectfolder/CalenderView.java
@@ -1,6 +1,11 @@
 package com.example.flowspace_projectfolder;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -35,16 +40,42 @@ public class CalenderView extends Application {
         VBox calendarBox = new VBox();
         Label calendarLabel = new Label("Calendar");
 
+        //calendar alignments
         GridPane calendarGrid = new GridPane();
         calendarGrid.setHgap(10);
         calendarGrid.setVgap(10);
+        calendarGrid.setAlignment(Pos.CENTER);
 
-        for (int row = 0; row < 5; row++) {
+    //calendar and functionality
+        //length of currentMonth
+        int curMonthLength = LocalDate.now().lengthOfMonth();
+        //first bracket (monday of this week)
+        int firstMonday = LocalDate.now()
+                .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY))
+                .getDayOfMonth();
+        int dayOffset = 0;
+        boolean wrap = false;
+        for (int row = 0; row < 4; row++) {
             for (int col = 0; col < 7; col++) {
+                //day logic for calendar
+                int day = firstMonday + dayOffset;
+                if (day > curMonthLength) {
+                    day = day - curMonthLength;
+                }
+                //create region for day
                 Region dayCell = new Region();
                 dayCell.setPrefSize(100, 100);
                 dayCell.setStyle("-fx-background-color: #d0ddff;");
-                calendarGrid.add(dayCell, col, row);
+                //create date-label for day
+                Label dayLabel = new Label(String.valueOf(day));
+                dayLabel.setTextFill(javafx.scene.paint.Color.GRAY);
+                StackPane.setAlignment(dayLabel, Pos.TOP_RIGHT);
+                dayLabel.setPadding(new Insets(5));
+                //stack region and day label
+                StackPane cellWithLabel = new StackPane(dayCell, dayLabel);
+
+                calendarGrid.add(cellWithLabel, col, row);
+                dayOffset++;
             }
         }
 
